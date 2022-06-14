ac9478c3c634aecd6894d5b0f73ee4844cecd3e2
Martekode <BrianDesmedt9000@hotmail.com>
2022-06-14 09:48:45 +0200

    added the files and wrote some test code

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PokemonPhp</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+ <img id="pokemonImg" src="" alt="pokemonImg">
+ <div class="info">
+    <h3 id="pokemonName">Pokemon</h3>
+    <p id="pokemonDiscription">Place Pokemon info here</p>
+ </div>   
+ <?php
+    echo "hello world";
+?>
+</body>
+</html>
