3f580facfb88d0e1e918d8a3d746b550078d216b
Martekode <BrianDesmedt9000@hotmail.com>
2022-06-14 11:31:39 +0200

    displayed the pokemon front_default img

--- index.php
+++ index.php
@@ -13,13 +13,22 @@
     $PokeFetch = file_get_contents($url);
     $PokeArray = json_decode($PokeFetch , true);
     #var_dump($PokeArray);
-    var_dump($PokeArray["name"]);
+    #var_dump($PokeArray["name"]);
+    $sprite = $PokeArray["sprites"]["front_default"];
     $pokeName = $PokeArray["name"];
+    #var_dump($sprite);
     $dom = new DOMDocument('1.0', 'utf-8');
     $element = $dom->createElement('h3', $pokeName);
     // We insert the new element as root (child of the document)
     $dom->appendChild($element);
+    $img = $dom->createElement('img', "");
+    $attr = $dom->createAttribute("src");
+    $tnode = $dom->createTextNode($sprite);
+    $attr->appendChild($tnode);
+    $img->appendChild($attr);
+    $dom->appendChild($img);
     echo $dom->saveXML();
+
 ?>
 </body>
 </html>
